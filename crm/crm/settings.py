4027ebebbb091f6eaf32cbe6e0d9eeeabaab9d1a
Yashraj Limkar <yashraj1416@gmail.com>
2025-07-12 10:16:09 +0530

    Changed Directory

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -140,4 +140,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # NOT your Gmail password
+
